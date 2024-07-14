b9cd8c029910fa91b59ae92592c08d1631c71f20
david-parkk <ji0513ji@naver.com>
2024-07-14 15:38:09 +0900

    feat : testRepository

---
+++ src/main/java/com/ourMenu/backend/test/TestEntity.java
@@ -0,0 +1,23 @@
+package com.ourMenu.backend.test;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode
+public class TestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
