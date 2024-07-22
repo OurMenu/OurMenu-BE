56c71c114e8b230046bc16f921c4f60cd079d76c
david-parkk <ji0513ji@naver.com>
2024-07-22 13:26:17 +0900

    feat: swagger domain

--- src/main/java/com/ourMenu/backend/global/config/SwaggerConfig.java
+++ src/main/java/com/ourMenu/backend/global/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.ourMenu.backend.global.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -9,8 +10,15 @@ import org.springframework.context.annotation.Configuration;
                 title = "ourmenu API 명세서",
                 description = "ourmenu 사용되는 API 명세서",
                 version = "v1"
-        )
+        ),
+        servers = {
+                @Server(
+                        url = "https://bluesparrow.shop",
+                        description = "HTTPS Server"
+                )
+        }
 )
+
 @Configuration
 public class SwaggerConfig {
 
