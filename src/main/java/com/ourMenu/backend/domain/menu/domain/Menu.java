e3f0a8344b801c520015d0b40f4318ebbd545efa
You-Hyuk <cxz6411@naver.com>
2024-07-24 21:30:03 +0900

    feat: createdAt, modifiedAt

--- src/main/java/com/ourMenu/backend/domain/menu/domain/Menu.java
+++ src/main/java/com/ourMenu/backend/domain/menu/domain/Menu.java
@@ -1,17 +1,16 @@
 package com.ourMenu.backend.domain.menu.domain;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import java.security.Timestamp;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
-
 public class Menu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,13 +25,24 @@ public class Menu {
     private String ImgUrl;
 
     @Enumerated(EnumType.STRING)
-    private MenuStatus status;
+    private MenuStatus status = MenuStatus.CREATED;
 
     @OneToMany(mappedBy = "menu")
     private List<MenuMenuList> menuMenuLists = new ArrayList<>();
 
-    private Timestamp createdAt;
-    private Timestamp modifiedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private String memo;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.status = (this.status == null) ? MenuStatus.CREATED : this.status;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedAt = LocalDateTime.now();
+    }
 
 }
