5ccc5ab69f03a9f5a8948dad55b3b98d3f6bfcf4
You-Hyuk <cxz6411@naver.com>
2024-07-25 22:33:52 +0900

    feat: 메뉴판 DTO 클래스

---
+++ src/main/java/com/ourMenu/backend/domain/menulist/dto/response/MenuListDto.java
@@ -0,0 +1,30 @@
+package com.ourMenu.backend.domain.menulist.dto.response;
+
+import com.ourMenu.backend.domain.menu.domain.MenuMenuList;
+import com.ourMenu.backend.domain.menulist.domain.MenuListStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+public class MenuListDto {
+
+    private Long id;
+
+    private MenuListStatus status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
+    private String title;
+
+    private String imgUrl;
+
+    private List<MenuMenuList> menuMenuLists = new ArrayList<>();
+
+}
