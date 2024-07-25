57db16fbd9021f71f91355dcaf1f6c7e2238e9f3
You-Hyuk <cxz6411@naver.com>
2024-07-25 22:39:21 +0900

    refactor: MenuDto 클래스 사용

--- src/main/java/com/ourMenu/backend/domain/menu/dto/response/MenuDto.java
+++ src/main/java/com/ourMenu/backend/domain/menu/dto/response/MenuDto.java
@@ -3,11 +3,10 @@ package com.ourMenu.backend.domain.menu.dto.response;
 import com.ourMenu.backend.domain.menu.domain.MenuStatus;
 import lombok.Data;
 
-import java.security.Timestamp;
 import java.time.LocalDateTime;
 
 @Data
-public class GetMenuResponse {
+public class MenuDto {
     private Long id;
     private String title;
     private int price;
