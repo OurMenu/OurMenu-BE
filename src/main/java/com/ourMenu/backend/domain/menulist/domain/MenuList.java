57f6367f4f951ac74b0c885baab5c6fd0fc59339
You-Hyuk <cxz6411@naver.com>
2024-07-25 22:34:18 +0900

    feat: 메뉴판 생성일 및 수정일 입력

--- src/main/java/com/ourMenu/backend/domain/menulist/domain/MenuList.java
+++ src/main/java/com/ourMenu/backend/domain/menulist/domain/MenuList.java
@@ -1,10 +1,12 @@
 package com.ourMenu.backend.domain.menulist.domain;
 
 import com.ourMenu.backend.domain.menu.domain.MenuMenuList;
+import com.ourMenu.backend.domain.menu.domain.MenuStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.security.Timestamp;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,19 +26,29 @@ public class MenuList {
     @Enumerated(EnumType.STRING)
     private MenuListStatus status;
 
-    private Timestamp createdAt;
-    private Timestamp modifiedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
     private String title;
 
     @Lob
     @Column(name = "image")
-    private String ImgUrl;
+    private String imgUrl;
 
 
     @OneToMany(mappedBy = "menuList", cascade = CascadeType.ALL)
     @Builder.Default
     private List<MenuMenuList> menuMenuLists = new ArrayList<>();
 
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.status = (this.status == null) ? MenuListStatus.CREATED : this.status;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.modifiedAt = LocalDateTime.now();
+    }
 
     //** 연관관계 메서드 **//
     public void addMenuMenuList(MenuMenuList menuMenuList){
