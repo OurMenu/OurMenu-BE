b8a95db32c2bed585e12f45efca7204b0846f31c
david-parkk <ji0513ji@naver.com>
2024-07-27 18:55:35 +0900

    feat: GET /community/{id}

--- src/main/java/com/ourMenu/backend/domain/dummy/DummyCommunityController.java
+++ src/main/java/com/ourMenu/backend/domain/dummy/DummyCommunityController.java
@@ -2,10 +2,9 @@ package com.ourMenu.backend.domain.dummy;
 
 import com.ourMenu.backend.global.common.ApiResponse;
 import com.ourMenu.backend.global.util.ApiUtils;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +30,36 @@ public class DummyCommunityController {
         return ApiUtils.success(getCommunity);
     }
 
+    @GetMapping("/community/{id}")
+    public ApiResponse<GetArticle>getArticle(@PathVariable Long id){
+        SimpleMenu simpleMenu1=new SimpleMenu(1L,"가락국수","남촌국수","https://hobbytat.s3.ap-northeast-2.amazonaws.com/dump/%ED%99%94%EC%82%B0%EB%9D%BC%EB%A9%98.png");
+        SimpleMenu simpleMenu2=new SimpleMenu(1L,"가락국수","남촌국수","https://hobbytat.s3.ap-northeast-2.amazonaws.com/dump/%ED%99%94%EC%82%B0%EB%9D%BC%EB%A9%98.png");
+        List<SimpleMenu>simpleMenus=List.of(simpleMenu1,simpleMenu2);
+        return ApiUtils.success(new GetArticle(1L,"제목이에요","내용이예요내용이예요내용이예요내용이예요내용이예요내용이예요내용이","김시진"
+                ,"https://hobbytat.s3.ap-northeast-2.amazonaws.com/dump/%ED%94%84%EB%A1%9C%ED%95%84+%EC%9D%B4%EB%AF%B8%EC%A7%80.jpg",123,simpleMenus));
+
+    }
+
+    @AllArgsConstructor
+    @Getter
+    private static class GetArticle{
+        private Long id;
+        private String title;
+        private String content;
+        private String creator;
+        private String profileImgUrl;
+        private int views;
+        List<SimpleMenu>articleMenus;
+    }
+    @AllArgsConstructor
+    @Getter
+    private static class SimpleMenu{
+        private Long id;
+        private String title;
+        private String placeName;
+        private String imgUrl;
+
+    }
 
     @AllArgsConstructor
     @Data
