77465f08c76bb897f91c21bc7550bf3bc26b4214
david-parkk <ji0513ji@naver.com>
2024-07-27 17:11:24 +0900

    fix: GET /home/recommend?questionId=&answer=

--- src/main/java/com/ourMenu/backend/domain/dummy/DummyHomeController.java
+++ src/main/java/com/ourMenu/backend/domain/dummy/DummyHomeController.java
@@ -41,13 +41,13 @@ public class DummyHomeController {
         return ApiUtils.success(responseList);
     }
 
-    @GetMapping("/home/recommended/")
+    @GetMapping("/home/recommended")
     public ApiResponse<DummyQnaResponse> dummyGetQna(){
         List<DummyQnaMenuDTO> list = new ArrayList<>();
-        list.add(new DummyQnaMenuDTO("url","화산돈부리","화산라멘 홍대점"));
-        list.add(new DummyQnaMenuDTO("url","화산돈부리","화산라멘 홍대점"));
+        list.add(new DummyQnaMenuDTO("https://hobbytat.s3.ap-northeast-2.amazonaws.com/dump/%ED%99%94%EC%82%B0%EB%9D%BC%EB%A9%98.png","화산돈부리","화산라멘 홍대점"));
+        list.add(new DummyQnaMenuDTO("https://hobbytat.s3.ap-northeast-2.amazonaws.com/dump/%ED%99%94%EC%82%B0%EB%9D%BC%EB%A9%98.png","화산돈부리","화산라멘 홍대점"));
 
-        DummyQnaResponse response = new DummyQnaResponse("url", list);
+        DummyQnaResponse response = new DummyQnaResponse("\"https://hobbytat.s3.ap-northeast-2.amazonaws.com/dump/%EC%8A%A4%ED%8A%B8%EB%A0%88%EC%8A%A4+%EB%B0%9B%EC%9D%84+%EB%95%8C+%EB%A7%A4%EC%BD%A4%ED%95%9C+%EC%9D%8C%EC%8B%9D+%EC%B6%94%EC%B2%9C+%EB%93%9C%EB%A0%A4%EC%9A%94.png\"", list);
         return ApiUtils.success(response);
     }
 }
