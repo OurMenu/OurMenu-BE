e060e6377f0e8e5c107389ddaa7e0f9d61a1d37f
david-parkk <ji0513ji@naver.com>
2024-07-27 17:12:10 +0900

    fix: GET /place

--- src/main/java/com/ourMenu/backend/domain/dummy/DummyPlaceController.java
+++ src/main/java/com/ourMenu/backend/domain/dummy/DummyPlaceController.java
@@ -18,9 +18,9 @@ public class DummyPlaceController {
     @GetMapping("/place")
     public ApiResponse<List<Place>> getAllplace(){
         List<Place> places = new ArrayList<>();
-        places.add(new Place(1, "화산라멘 홍대점", 3, 12312.2423, 12312.2423));
-        places.add(new Place(2, "화산라멘 강남점", 3, 12312.2424, 12312.2424));
-        places.add(new Place(3, "화산라멘 신촌점", 3, 12312.2425, 12312.2425));
+        places.add(new Place(1, "화산라멘 홍대점", 3, 1269258716, 375531589));
+        places.add(new Place(2, "화산라멘 강남점", 3, 1269258716, 375531589));
+        places.add(new Place(3, "화산라멘 신촌점", 3, 1269258716, 375531589));
         return ApiUtils.success(places);
     }
     @Data
