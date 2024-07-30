141fb844e1f1aa694bf2928d67d3eda39c0890cf
david-parkk <ji0513ji@naver.com>
2024-07-30 22:22:13 +0900

    feat: MissingServletRequestParameterException

--- src/main/java/com/ourMenu/backend/global/exception/ErrorCode.java
+++ src/main/java/com/ourMenu/backend/global/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,"G500","서버 내부에서 에러가 발생하였습니다"),
+    MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "G400", "요청 파라미터가 누락되었습니다"),
 
     //search
     SEARCH_RESULT_NOT_FOUND(HttpStatus.NOT_FOUND,"S404","검색 결과가 없습니다")
