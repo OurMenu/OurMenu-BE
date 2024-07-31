e2de176df7e4fccf25bc1e3a06f6b452de635594
Sang-Min Kang <rdrniel12345@gmail.com>
2024-08-01 05:55:01 +0900

    feat: add token logic in account service signup

--- src/main/java/com/ourMenu/backend/domain/user/application/AccountService.java
+++ src/main/java/com/ourMenu/backend/domain/user/application/AccountService.java
@@ -3,6 +3,7 @@ package com.ourMenu.backend.domain.user.application;
 import com.ourMenu.backend.domain.user.api.request.SignUpRequest;
 import com.ourMenu.backend.domain.user.api.response.LoginResponse;
 import com.ourMenu.backend.domain.user.dao.UserDao;
+import com.ourMenu.backend.global.util.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.time.Instant;
@@ -13,10 +14,13 @@ public class AccountService {
 
     private final String GRANT_TYPE = "Bearer";
     private final UserDao userDao;
+    private final JwtProvider jwtProvider;
 
     public LoginResponse signup(SignUpRequest request) {
         long id = userDao.createUser(request);
-        return new LoginResponse(GRANT_TYPE, "", "", Instant.now());
+        String accessToken = jwtProvider.createToken(id, 1);
+        String refreshToken = jwtProvider.createToken(id, 24 * 30);
+        return new LoginResponse(GRANT_TYPE, accessToken, refreshToken, Instant.now());
     }
 
 }
