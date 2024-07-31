a651620f0968fcccf24cfeac9b984ae438d4d14d
Sang-Min Kang <rdrniel12345@gmail.com>
2024-07-31 20:33:53 +0900

    feat: add redis password

--- src/main/java/com/ourMenu/backend/global/config/RedisConfig.java
+++ src/main/java/com/ourMenu/backend/global/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
@@ -13,10 +14,16 @@ public class RedisConfig {
     private String host;
     @Value("${spring.redis.port}")
     private int port;
+    @Value("${REDIS_PASSWORD}")
+    private String password;
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
     }
 
 }
