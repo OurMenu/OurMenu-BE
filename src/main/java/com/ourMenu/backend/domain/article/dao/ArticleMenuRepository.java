b606475194ca822659f2f5722f7f0bd827e926b8
david-parkk <ji0513ji@naver.com>
2024-08-01 15:34:44 +0900

    fix: articleMenuRepository

--- src/main/java/com/ourMenu/backend/domain/article/dao/ArticleMenuRepository.java
+++ src/main/java/com/ourMenu/backend/domain/article/dao/ArticleMenuRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ArticleMenuRepository extends JpaRepository<ArticleMenu, String> {
+public interface ArticleMenuRepository extends JpaRepository<ArticleMenu, Long> {
 }
