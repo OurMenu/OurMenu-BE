7548e0838c67e97ceea649c5241163cc3d436fd8
You-Hyuk <cxz6411@naver.com>
2024-08-01 19:23:05 +0900

    feat: DTO 클래스 및 변수 변경

---
+++ src/main/java/com/ourMenu/backend/domain/menulist/dto/response/GetMenuListResponse.java
@@ -0,0 +1,13 @@
+package com.ourMenu.backend.domain.menulist.dto.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class GetMenuListResponse {
+    private String title;
+    private Long menuCount;
+    private String imgUrl;
+    private String iconType;
+}
