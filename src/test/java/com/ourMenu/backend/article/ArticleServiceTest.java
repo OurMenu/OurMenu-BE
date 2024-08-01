7f71c2b16ed7c251eb43292b0472b7f002965249
david-parkk <ji0513ji@naver.com>
2024-08-01 15:45:47 +0900

    feat: test

--- src/test/java/com/ourMenu/backend/article/ArticleServiceTest.java
+++ src/test/java/com/ourMenu/backend/article/ArticleServiceTest.java
@@ -1,9 +1,11 @@
 package com.ourMenu.backend.article;
 
 import com.ourMenu.backend.domain.article.application.ArticleService;
+import com.ourMenu.backend.domain.article.dao.ArticleMenuRepository;
 import com.ourMenu.backend.domain.article.domain.Article;
 import com.ourMenu.backend.domain.article.domain.ArticleMenu;
 import com.ourMenu.backend.domain.article.exception.NoSuchArticleException;
+import com.ourMenu.backend.domain.article.exception.NoSuchArticleMenuException;
 import com.ourMenu.backend.global.common.Status;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -65,8 +67,8 @@ public class ArticleServiceTest {
                 .content("게시글 내용")
                 .articleMenuList(articleMenuList)
                 .build();
-        //when
         Article saveArticle = articleService.save(article);
+        //when
         Article softDeleteArticle = articleService.softDelete(saveArticle.getId());
         //then
         Assertions.assertThat(softDeleteArticle.getStatus()).isEqualTo(Status.DELETED);
@@ -86,6 +88,60 @@ public class ArticleServiceTest {
         });
         //then
         String expectedMessage = "해당하는 게시물이 없습니다";
+        String actualMessage = exception.getMessage();
+        Assertions.assertThat(expectedMessage).isEqualTo(actualMessage);
+    }
+
+    @DisplayName("hard 삭제를 할 수 있다")
+    @Test
+    @Transactional
+    public void test4() {
+        //given
+        List<ArticleMenu> articleMenuList = List.of(articleMenu1, articleMenu2);
+        Article article = Article.builder()
+                .title("게시글 제목")
+                .content("게시글 내용")
+                .articleMenuList(articleMenuList)
+                .build();
+        Article saveArticle = articleService.save(article);
+
+        //when
+        articleService.hardDelete(saveArticle.getId());
+        RuntimeException exception = org.junit.jupiter.api.Assertions.assertThrows(NoSuchArticleException.class, () -> {
+            Article softDeleteArticle = articleService.softDelete(1L);
+            Assertions.assertThat(softDeleteArticle.getStatus()).isEqualTo(Status.DELETED);
+        });
+
+        //then
+        String expectedMessage = "해당하는 게시물이 없습니다";
+        String actualMessage = exception.getMessage();
+        Assertions.assertThat(expectedMessage).isEqualTo(actualMessage);
+
+    }
+
+    @DisplayName("hard 삭제시 게시글과 게시글 메뉴 모두 삭제되어야한다")
+    @Test
+    @Transactional
+    public void test5() {
+        //given
+        List<ArticleMenu> articleMenuList = List.of(articleMenu1, articleMenu2);
+        Article article = Article.builder()
+                .title("게시글 제목")
+                .content("게시글 내용")
+                .articleMenuList(articleMenuList)
+                .build();
+        Article saveArticle = articleService.save(article);
+
+        //when
+        articleService.hardDelete(saveArticle.getId());
+        RuntimeException exception = org.junit.jupiter.api.Assertions.assertThrows(NoSuchArticleMenuException.class, () -> {
+            Long id = article.getArticleMenuList().get(0).getId();
+            articleService.findArticleMenu(id);
+
+        });
+
+        //then
+        String expectedMessage = "해당하는 게시물메뉴가 없습니다";
         String actualMessage = exception.getMessage();
         Assertions.assertThat(expectedMessage).isEqualTo(actualMessage);
 
