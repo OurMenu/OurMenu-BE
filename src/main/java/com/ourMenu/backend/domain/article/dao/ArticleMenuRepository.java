2c7cced5cbeeaa353b9ee538202b45a07b0ebc2a
david-parkk <ji0513ji@naver.com>
2024-08-01 14:26:37 +0900

    feat: articel repository

---
+++ src/main/java/com/ourMenu/backend/domain/article/dao/ArticleMenuRepository.java
@@ -0,0 +1,9 @@
+package com.ourMenu.backend.domain.article.dao;
+
+import com.ourMenu.backend.domain.article.domain.ArticleMenu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleMenuRepository extends JpaRepository<ArticleMenu, String> {
+}
