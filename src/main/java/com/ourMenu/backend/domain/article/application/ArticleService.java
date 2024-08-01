934f2a1630e35f5f67639d211ca4accdc9b44855
david-parkk <ji0513ji@naver.com>
2024-08-01 15:24:04 +0900

    feat: hard delete

--- src/main/java/com/ourMenu/backend/domain/article/application/ArticleService.java
+++ src/main/java/com/ourMenu/backend/domain/article/application/ArticleService.java
@@ -39,4 +39,11 @@ public class ArticleService {
         article.setStatus(Status.DELETED);
         return article;
     }
+
+    @Transactional
+    public void hardDelete(Long id){
+        Article article = articleRepository.findById(id)
+                .orElseThrow(()->new NoSuchArticleException("해당하는 게시물이 없습니다"));
+        articleRepository.delete(article);
+    }
 }
