249671a5459f3c0ca0f8be80966764546aa88bfa
david-parkk <ji0513ji@naver.com>
2024-08-08 04:56:30 +0900

    fix: response

--- src/main/java/com/ourMenu/backend/domain/onboarding/api/OnBoardingController.java
+++ src/main/java/com/ourMenu/backend/domain/onboarding/api/OnBoardingController.java
@@ -2,10 +2,13 @@ package com.ourMenu.backend.domain.onboarding.api;
 
 import com.ourMenu.backend.domain.menu.domain.Menu;
 import com.ourMenu.backend.domain.onboarding.api.response.GetOnboardingResponse;
+import com.ourMenu.backend.domain.onboarding.api.response.GetQuestionRecommands;
 import com.ourMenu.backend.domain.onboarding.application.OnBoardingService;
 import com.ourMenu.backend.domain.onboarding.domain.AnswerType;
 import com.ourMenu.backend.domain.onboarding.domain.Question;
 import com.ourMenu.backend.global.argument_resolver.UserId;
+import com.ourMenu.backend.global.common.ApiResponse;
+import com.ourMenu.backend.global.util.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,18 +25,18 @@ public class OnBoardingController {
     private final OnBoardingService onBoardService;
 
     @GetMapping
-    public List<GetOnboardingResponse> getOnboarding() {
+    public ApiResponse<List<GetOnboardingResponse>> getOnboarding() {
         List<Question> allQuestion = onBoardService.getAllQuestion();
-        return allQuestion.stream().map(GetOnboardingResponse::toDto).toList();
-
+        List<GetOnboardingResponse> list = allQuestion.stream().map(GetOnboardingResponse::toDto).toList();
+        return ApiUtils.success(list);
     }
 
     @GetMapping("/recommand")
-    public String getQuestionRecommand(@RequestParam("questionId") int questionId,
-                                       @RequestParam("answer") AnswerType answerType,
-                                       @UserId Long userId) {
+    public ApiResponse<GetQuestionRecommands> getQuestionRecommand(@RequestParam("questionId") int questionId,
+                                            @RequestParam("answer") AnswerType answerType,
+                                            @UserId Long userId) {
         List<Menu> menus = onBoardService.findStoreByQuestionAnswer(userId, questionId, answerType);
+        return ApiUtils.success(GetQuestionRecommands.toDto(menus,questionId,answerType));
 
-        return "success";
     }
 }
