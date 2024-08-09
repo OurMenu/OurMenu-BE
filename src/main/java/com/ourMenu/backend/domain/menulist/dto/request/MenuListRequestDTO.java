d810a9610410196a669516ef461c67206454ecce
You-Hyuk <cxz6411@naver.com>
2024-08-09 13:20:01 +0900

    fix: Swagger MultipartFile null 허용

--- src/main/java/com/ourMenu/backend/domain/menulist/dto/request/MenuListRequestDTO.java
+++ src/main/java/com/ourMenu/backend/domain/menulist/dto/request/MenuListRequestDTO.java
@@ -1,5 +1,6 @@
 package com.ourMenu.backend.domain.menulist.dto.request;
 
+ import io.swagger.v3.oas.annotations.media.Schema;
  import lombok.Builder;
  import lombok.Data;
  import org.springframework.web.multipart.MultipartFile;
@@ -10,7 +11,10 @@ package com.ourMenu.backend.domain.menulist.dto.request;
 @Builder
 public class MenuListRequestDTO {
 //    private String img;
+
+    @Schema(description = "메뉴판 이미지 파일", nullable = true)
     private Optional<MultipartFile> menuFolderImg;    //파일로 받아야 함
+
     private String menuFolderTitle;
     private String menuFolderIcon;
     private List<Long> menuIds;
