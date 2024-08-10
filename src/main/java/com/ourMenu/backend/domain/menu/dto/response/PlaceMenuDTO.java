0de71d17ebf80bf92e7254b32a9699bfa8c7dc01
You-Hyuk <cxz6411@naver.com>
2024-08-10 23:39:17 +0900

    feat: 메뉴Id 제거 및 메뉴판 정보 추가

--- src/main/java/com/ourMenu/backend/domain/menu/dto/response/PlaceMenuDTO.java
+++ src/main/java/com/ourMenu/backend/domain/menu/dto/response/PlaceMenuDTO.java
@@ -9,12 +9,12 @@ import java.util.List;
 @Data
 @Builder
 public class PlaceMenuDTO {
-    private Long menuId;
+//    private Long menuId;
     private Long groupId;
     private String menuTitle;
     private int menuPrice;
     private String menuIcon;
     private List<TagDTO> menuTags;
     private List<MenuImageDto> menuImgsUrl;
-    private PlaceMenuFolderDTO menuFolder;
+    private List<PlaceMenuFolderDTO> menuFolder;
 }
