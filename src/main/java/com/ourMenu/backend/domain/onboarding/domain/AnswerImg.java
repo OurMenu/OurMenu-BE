1d7f39595a3ed789ae990147093ac70685f5650d
david-parkk <ji0513ji@naver.com>
2024-08-15 12:51:23 +0900

    fix: 누락 img 추가

--- src/main/java/com/ourMenu/backend/domain/onboarding/domain/AnswerImg.java
+++ src/main/java/com/ourMenu/backend/domain/onboarding/domain/AnswerImg.java
@@ -13,8 +13,8 @@ public enum AnswerImg {
     SWEET("스트레스 해소엔 달달한게 최고지", "스트레스 만땅 달달함으로 풀어볼까요"),
     SPICY("스트레스 해소엔 역시 불닭으로 화끈하게"),
     SEA("바다 내음이 물씬 풍기는 해산물 어때요1", "바다 내음이 물씬 풍기는 해산물 어때요2"),
-    MOUNTAIN("피톤치드 향 가득 느낄 수 있는 건강식 어때요", "무더운 여름엔 이열치열로 땀좀 빼 볼까요"),
-    SUMMER("따가운 햇살엔 이열치열! 뜨거운 음식들", "햇살이 뜨거울때 차가운 음식들로 이겨내 볼까요"),
+    MOUNTAIN("피톤치드 향 가득 느낄 수 있는 건강식 어때요"),
+    SUMMER("무더운 여름엔 이열치열로 땀좀 빼 볼까요", "따가운 햇살엔 이열치열! 뜨거운 음식들", "햇살이 뜨거울때 차가운 음식들로 이겨내 볼까요"),
     WINTER("추운 겨울엔 뜨끈한 국물 어때요", "추운 겨울의 따끈따근한 제철 음식이에요"),
     ;
 
@@ -27,6 +27,6 @@ public enum AnswerImg {
     public String getRandomImgUrl() {
         Random random = new Random();
         int randomValue = random.nextInt(imgUrlList.size());
-        return S3Util.S3Path + imgUrlList.get(randomValue) + ".svg";
+        return S3Util.getS3SvgPath(imgUrlList.get(randomValue));
     }
 }
