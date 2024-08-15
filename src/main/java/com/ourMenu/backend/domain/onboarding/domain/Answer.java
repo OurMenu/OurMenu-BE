fcf9d894f1c82b0f838f325c27827f164c3124e8
david-parkk <ji0513ji@naver.com>
2024-08-15 12:18:56 +0900

    fix: 오타수정

--- src/main/java/com/ourMenu/backend/domain/onboarding/domain/Answer.java
+++ src/main/java/com/ourMenu/backend/domain/onboarding/domain/Answer.java
@@ -10,13 +10,13 @@ public enum Answer {
     FEELING("좋아!", S3Util.S3Path + "좋아.svg", S3Util.S3Path + "기분이 꿀꿀할때는 돼지 파티다.svg", AnswerFood.LIKE,
             "별로야..", S3Util.S3Path + "별로야.svg", S3Util.S3Path + "기분이 저기압일 땐 고기 앞으로 가라.svg", AnswerFood.DISLIKE),
     WEATHER("맑아", S3Util.S3Path + "맑아.svg", S3Util.S3Path + "날씨도 맑은데 바깥에서 피크닉 어때요_.svg", AnswerFood.SUNNY,
-            "비가 오네", S3Util.S3Path + "비가와.svg", S3Util.S3Path + "비오고 쌀쌀할 때 뜨끈한 국물 어때요_.svg", AnswerFood.RAINNY),
+            "비가 오네", S3Util.S3Path + "비가와.svg", S3Util.S3Path + "비오고 쌀쌀할 때 뜨끈한 국물 어때요_.svg", AnswerFood.RAINY),
     STRESS("달달한 음식을 먹어", S3Util.S3Path + "달달한음식.svg", S3Util.S3Path + "스트레스 해소엔 달달한게 최고지~.svg", AnswerFood.SWEET,
             "매운 음식이지!", S3Util.S3Path + "매운음식.svg", S3Util.S3Path + "스트레스 해소엔 역시 화끈하게!.svg", AnswerFood.SPICY),
     TRAVEL("바다", S3Util.S3Path + "바다.svg", S3Util.S3Path + "바다 내음이 물씬나는 해산물 어때요_.svg", AnswerFood.SEA,
             "산", S3Util.S3Path + "산.svg", S3Util.S3Path + "피톤치드향 가득한 건강식 어때요_.svg", AnswerFood.MOUNTAIN),
     SEASON("여름", S3Util.S3Path + "여름.svg", S3Util.S3Path + "따가운 햇살엔 이열치열! 뜨거운 음식들.svg", AnswerFood.SUMMER,
-            "겨울", S3Util.S3Path + "겨울.svg", S3Util.S3Path + "따가운 햇살을 이겨내는 차가운 음식들 어때요_.svg", AnswerFood.WINNTER),
+            "겨울", S3Util.S3Path + "겨울.svg", S3Util.S3Path + "따가운 햇살을 이겨내는 차가운 음식들 어때요_.svg", AnswerFood.WINTER),
     ;
 
     private final String yes;
